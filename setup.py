f9f9ad4bcec2f8fc47e91ed34c82bfaffb899ee5
Brian Gomberg <brian@audigolabs.com>
2020-08-18 13:26:21 -0700

    Just install cc3220 tool

--- setup.py
+++ setup.py
@@ -2,17 +2,15 @@ from distutils.core import setup
 import setuptools
 
 setup(
-    name="cc3200tool",
+    name="cc3220tool",
     version="0.2.0",
-    description="A tool to upload files to TI CC32xx ",
+    description="A tool to upload files to TI CC3220 ",
     author="Kiril Zyapkov",
     author_email="k.zyapkov@allterco.com",
-    url="http://github.com/s1ider/cc3200tool.git",
+    url="https://github.com/AudigoLabs/cc3200tool",
     packages=setuptools.find_packages(),
-    package_data={'cc3200tool': ['dll/*.dll'],
-                  'cc3220tool': ['dll/gen2/*.ptc']},
+    package_data={'cc3220tool': ['dll/gen2/*.ptc']},
     scripts=[
-                'scripts/cc3200tool',
                 'scripts/cc3220tool',
             ],
     install_requires=['pyserial', 'progress'],
